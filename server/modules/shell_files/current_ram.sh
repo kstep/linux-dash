95eb3c6699923777c30bbe7811b1c22b628b68b4
Konstantin Stepanov <me@kstep.me>
2015-07-16 01:30:01 +0300

    fix memory stats module

--- server/modules/shell_files/current_ram.sh
+++ server/modules/shell_files/current_ram.sh
@@ -6,7 +6,7 @@ awkCmd=`which awk`
 procps=$($freeCmd -V | /bin/grep procps-ng)
 
 if [ -z "$procps" ]; then
-	$freeCmd -tmo | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3-$6-$7 ", \"free\": " $4+$6+$7 " }"}'
+	$freeCmd -tmo | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3 ", \"free\": " $4+$6 " }"}'
 else
-	$freeCmd -tm | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3-$6-$7 ", \"free\": " $4+$6+$7 " }"}'
+	$freeCmd -tm | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3 ", \"free\": " $4+$6 " }"}'
 fi
